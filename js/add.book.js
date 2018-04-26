5754d4b7544e1a9a17f4be680250cdd2ba751c23
urgupluoglu@gmail.com <yusuf.urgupluoglu@airties.com>
2018-04-26 09:17:39 +0300

    click event'inin popper js elementi uzerinde ufak bir test kodu eklendi inline yazmiyoruz, css dosyasini dogru sekilde kullaniyoruz

--- js/add.book.js
+++ js/add.book.js
@@ -8,4 +8,16 @@ $(document).ready(function() {
 			});
 		}
 	});
+
+	var i=1;
+	$(".ekle").click(function() {
+		var kitapEkleHtml = "<div class='kitapEkleDiv'><input class='kitapEkleInput form-control' placeholder='"+i+""+'.Kitabı Ekle+'+"' type='text'><a href='#'><button class='ekle btn btn-outline-success my-2 my-sm-0' type='button'>Kitaplara Ekle+</button></a></div>";
+		$(".ekledeneme").before(kitapEkleHtml);
+		i++;
+	});
+
+	$('#genre a').click(function() {
+		// this is just an example of click event handling
+		$('#genreButton').html($(this).html());
+	});
 });
